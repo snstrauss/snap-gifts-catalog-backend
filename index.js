090f952dffc6cd965ed3174a909ee174d0a5b89e
snstrauss <zahavi.guy.85@gmail.com>
2020-08-13 14:55:03 +0300

    define CORS headers

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const DATA = require('./data.json');
 
 const app = express();
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 app.get('/products', (req, res) => {
 
     console.log('GET PRODUCTS.');
