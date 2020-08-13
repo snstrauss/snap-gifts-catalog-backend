90463f80a19b8aecdef8278ccca0e7d0c6732e4d
snstrauss <zahavi.guy.85@gmail.com>
2020-08-13 18:17:45 +0300

    app now uses the services

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 const express = require('express');
-const DATA = require('./data.json');
+
+const cacheService = require('./services/cache.service');
+const { getProductsFromCache, getPromotions, getVendors } = require('./services/product.service');
 
 const PORT = 5000;
 const CACHE_TIMEOUT_MINS = 5;
 const CACHE_TIMEOUT = CACHE_TIMEOUT_MINS * 60 * 1000;
-const DEFAULT_CACHE_KEY = 'ALL';
-
-const QUERY_CACHE = {};
 
 const app = express();
 
@@ -16,55 +15,20 @@ app.use(function(req, res, next) {
     next();
 });
 
-
-function isMeaningfulQuery(q){
-    return q !== DEFAULT_CACHE_KEY;
-}
-
-function getProducts(query){
-    const queryRgx = new RegExp(query.toLowerCase());
-
-    console.log('GET PRODUCTS: ' + query);
-
-    return isMeaningfulQuery(query)
-            ? DATA.products.filter(prod => queryRgx.test(prod.name.toLowerCase()))
-            : DATA.products;
-}
-
-function clearCache(name){
-
-    console.log('DELETE CACHE: ' + name);
-
-    delete QUERY_CACHE[name];
-}
-
 app.get('/products', (req, res) => {
+    const query = req.query.query.length ? req.query.query : cacheService.CONSTS.DEFAULT.CACHE_KEY;
 
-    const query = req.query.query.length ? req.query.query : DEFAULT_CACHE_KEY;
-
-    if(!QUERY_CACHE[query]){
-        // using Promise.resolve to simulate data coming from DB,
-        // and to use it for caching
-        QUERY_CACHE[query] = Promise.resolve(getProducts(query));
-    }
-
-    if(isMeaningfulQuery(query)){
-        setTimeout(() => {
-            clearCache(query);
-        }, CACHE_TIMEOUT);
-    }
-
-    QUERY_CACHE[query].then(cachedResponse => {
-        res.json(cachedResponse);
+    getProductsFromCache(query, CACHE_TIMEOUT).then(productsData => {
+        res.json(productsData);
     });
 });
 
-app.get('/promotions', (req, res) => {
-    res.json(Array.isArray(DATA.promotion) ? DATA.promotion : [DATA.promotion]);
+app.get('/promotions', (_, res) => {
+    res.json(getPromotions());
 });
 
-app.get('/vendors', (req, res) => {
-    res.json(DATA.vendors);
+app.get('/vendors', (_, res) => {
+    res.json(getVendors());
 });
 
 app.listen(process.env.PORT || PORT, () => {
